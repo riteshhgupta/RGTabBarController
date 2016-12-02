4b6ca2574e5ff41abf40a76071634efb69ae41bb
Ritesh Gupta <rg.riteshh@gmail.com>
2016-12-02 15:21:35 +0530

    refactor(page-controller): better functions

--- Source/RGPageController.swift
+++ Source/RGPageController.swift
@@ -14,13 +14,11 @@ typealias Closure<T> = (T) -> Void
 class RGPageController: UIPageViewController {
 	
 	let controllers: [UIViewController]
-	var scrollView: UIScrollView?
 	var currentPage: Int = 0 {
 		didSet {
 			didChangePage?(currentPage)
 		}
 	}
-	var potentialNextIndex: Int = 0
 	
 	var didChangePage: Closure<Int>?
 	var didChangePageContentOffset: Closure<CGFloat>?
@@ -31,29 +29,10 @@ class RGPageController: UIPageViewController {
 		self.controllers = controllers
 		super.init(transitionStyle: .scroll, navigationOrientation: .horizontal)
 		guard !controllers.isEmpty else { return }
-		
 		delegate = self
 		dataSource = self
 		
-		setViewControllers(
-			[controllers[0]],
-			direction: .forward,
-			animated: true,
-			completion: nil
-		)
-	}
-	
-	func move(toPage page: Int) {
-		guard currentPage != page else { return }
-		let direction: UIPageViewControllerNavigationDirection = (page > currentPage) ? .forward : .reverse
-		shouldObserveForContentOffsetChange = false
-		currentPage = page
-		setViewControllers(
-			[controllers[page]],
-			direction: direction,
-			animated: true,
-			completion: nil
-		)
+		show(viewController: controllers[0])
 	}
 	
 	override func viewDidLoad() {
@@ -62,12 +41,23 @@ class RGPageController: UIPageViewController {
 	
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
-		scrollView = view.subviews.filter { $0 is UIScrollView }.first as? UIScrollView
 		scrollView?.delegate = self
 	}
 	
 	required init?(coder aDecoder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
+	}
+	
+	func move(toPage page: Int) {
+		guard currentPage != page else { return }
+		shouldObserveForContentOffsetChange = false
+		currentPage = page
+		let dr = direction(forNextPage: page)
+		show(viewController: controllers[page], inDirection: dr)
+	}
+	
+	func direction(forNextPage page: Int) -> UIPageViewControllerNavigationDirection {
+		return (page > currentPage) ? .forward : .reverse
 	}
 }
 
@@ -75,10 +65,10 @@ extension RGPageController: UIScrollViewDelegate {
 	
 	func scrollViewDidScroll(_ scrollView: UIScrollView) {
 		let currentOffset = scrollView.contentOffset.x
-		let delta = (currentOffset - previousContentOffset)/CGFloat(controllers.count)
 		previousContentOffset = currentOffset
-
+		
 		if shouldObserveForContentOffsetChange {
+			let delta = (currentOffset - previousContentOffset)/CGFloat(controllers.count)
 			didChangePageContentOffset?(delta)
 		}
 	}
@@ -93,9 +83,6 @@ extension RGPageController: UIScrollViewDelegate {
 }
 
 extension RGPageController: UIPageViewControllerDelegate {
-	
-	func pageViewController(_ pageViewController: UIPageViewController, willTransitionTo pendingViewControllers: [UIViewController]) {
-	}
 	
 	func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
 		currentPage += pageViewController.pageFactor
@@ -117,10 +104,27 @@ extension RGPageController: UIPageViewControllerDataSource {
 extension UIPageViewController {
 	
 	var pageFactor: Int {
-		let sv = view.subviews.filter { $0 is UIScrollView }.first as? UIScrollView
-		guard let scrollView = sv else { return 0 }
+		guard let scrollView = scrollView else { return 0 }
 		if scrollView.contentOffset.x < 375.0 { return -1 }
 		else if scrollView.contentOffset.x > 375.0 { return 1 }
 		return 0
 	}
+	func show(viewController: UIViewController, inDirection direction: UIPageViewControllerNavigationDirection = .forward) {
+		setViewControllers([viewController], direction: direction, animated: true, completion: nil)
+	}
+	var scrollView: UIScrollView? {
+		return view.subviews.filter { $0 is UIScrollView }.first as? UIScrollView
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
